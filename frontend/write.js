a0e327242495b2d9e7d6a749cf28660ffd5d54cd
ParkJaeHyung97 <wogud7575@naver.com>
2024-04-16 01:22:49 +0900

    복구 완료...
    복구 한듯.. 근데 시간이 안뜸

--- frontend/write.js
+++ frontend/write.js
@@ -1,9 +1,13 @@
+const form = document.getElementById("write-form");
+
 const handleSumitForm = async (event) => {
   event.preventDefault();
+  const body = new FormData(form);
+  body.append('insertAt',new Date().getTime());
   try {
     const res = await fetch("/items", {
       method: "POST",
-      body: new FormData(form),
+      body:body,
     });
     const data = await res.json();
     if (data === "200") window.location.pathname = "/";
@@ -11,5 +15,5 @@ const handleSumitForm = async (event) => {
     console.error("이미지 업로드에 실패했습니다.");
   }
 };
-const form = document.getElementById("write-form");
+
 form.addEventListener("submit", handleSumitForm);
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI,UploadFile,Form
+from fastapi import FastAPI,UploadFile,Form,Response
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 import sqlite3
@@ -14,19 +16,21 @@ async def create_item(image:UploadFile,
                 title:Annotated[str,Form()] ,
                 price:Annotated[int,Form()],
                 description:Annotated[str,Form()],
-                place:Annotated[str,Form()]):
+                place:Annotated[str,Form()],
+                insertAt:Annotated[int,Form()]
+                ):
     
     image_bytes = await image.read()
     cur.execute(f"""
-                INSERT INTO items(title,image,price,description,place)
-                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}')
+                INSERT INTO items(title,image,price,description,place,insertAt)
+                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}','{insertAt}')
                 """)
     con.commit()
     return '200'
 
 @app.get('/items')
 async def get_items():
-    con.row_factory = sqlite3.Row
+    con.row_factory = sqlite3.Row #컬럼명도 같이 가져오는 방법 {id:1 , title:빵팔아요 ...}
     cur = con.cursor()
     rows = cur.execute(f"""
                        SELECT * from items;
@@ -40,5 +44,6 @@ async def get_image(item_id):
     image_bytes = cur.execute(f"""
                               SELECT image from items WHERE id={item_id}
                               """).fetchone()[0]
+    return Response(content=bytes.fromhex(image_bytes))
 
 app.mount("/", StaticFiles(directory="frontend", html=True),name="frontend")
