31167a57e6b895b710a203abd2f3b103098bc545
ParkJaeHyung97 <wogud7575@naver.com>
2024-04-19 02:23:09 +0900

    svelte부터 다시 시작
    svelte 적용하여 페이지 옮겨왔음.

---
+++ src/pages/Main.svelte
@@ -0,0 +1,131 @@
+<script>
+  let hour = new Date().getHours();
+  let min = new Date().getMinutes();
+</script>
+
+<header>
+  <div class="info-bar">
+    <div class="info-bar__time">{hour}:{min}</div>
+    <div class="info-bar__icons">
+      <img src="Assets/chart-bar.svg" alt="Chart-bar" />
+      <img src="Assets/wifi.svg" alt="Wifi" />
+      <img src="Assets/battery.svg" alt="Battery" />
+    </div>
+  </div>
+  <div class="menu-bar">
+    <div class="menu-bar__location">
+      <div>내손1동</div>
+      <div class="menu-bar__location-icon">
+        <img src="Assets/arrow-down.svg" alt="" />
+      </div>
+    </div>
+    <div class="menu-bar__icons">
+      <img src="Assets/search.svg" alt="serach" />
+      <img src="Assets/menu.svg" alt="menu" />
+      <img src="Assets/bell.svg" alt="bell" />
+    </div>
+  </div>
+</header>
+
+<main>
+  <div class="item-list">
+    <div class="item-list__img">
+      <img src="Assets/img.svg" alt="" />
+    </div>
+    <div class="item-list__info">
+      <div class="item-list__info-title">게이밍 PC 팝니다</div>
+      <div class="item-list__info-meta">내손1동 19초 전</div>
+      <div class="item-list__info-price">100만원</div>
+    </div>
+  </div>
+  <div class="item-list">
+    <div class="item-list__img">
+      <img src="Assets/img.svg" alt="" />
+    </div>
+    <div class="item-list__info">
+      <div class="item-list__info-title">게이밍 PC 팝니다</div>
+      <div class="item-list__info-meta">내손1동 19초 전</div>
+      <div class="item-list__info-price">100만원</div>
+    </div>
+  </div>
+  <div class="item-list">
+    <div class="item-list__img">
+      <img src="Assets/img.svg" alt="" />
+    </div>
+    <div class="item-list__info">
+      <div class="item-list__info-title">게이밍 PC 팝니다</div>
+      <div class="item-list__info-meta">내손1동 19초 전</div>
+      <div class="item-list__info-price">100만원</div>
+    </div>
+  </div>
+  <div class="item-list">
+    <div class="item-list__img">
+      <img src="Assets/img.svg" alt="" />
+    </div>
+    <div class="item-list__info">
+      <div class="item-list__info-title">게이밍 PC 팝니다</div>
+      <div class="item-list__info-meta">내손1동 19초 전</div>
+      <div class="item-list__info-price">100만원</div>
+    </div>
+  </div>
+  <div class="item-list">
+    <div class="item-list__img">
+      <img src="Assets/img.svg" alt="" />
+    </div>
+    <div class="item-list__info">
+      <div class="item-list__info-title">게이밍 PC 팝니다</div>
+      <div class="item-list__info-meta">내손1동 19초 전</div>
+      <div class="item-list__info-price">100만원</div>
+    </div>
+  </div>
+  <div class="item-list">
+    <div class="item-list__img">
+      <img src="Assets/img.svg" alt="" />
+    </div>
+    <div class="item-list__info">
+      <div class="item-list__info-title">게이밍 PC 팝니다</div>
+      <div class="item-list__info-meta">내손1동 19초 전</div>
+      <div class="item-list__info-price">100만원</div>
+    </div>
+  </div>
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
+</main>
+
+<footer>
+  <div class="footer-block">
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="Assets/home.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">홈</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="Assets/paper.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">동네생활</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="Assets/gps.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">내 근처</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="Assets/chat.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">채팅</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="Assets/user.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">나의 당근</div>
+    </div>
+  </div>
+</footer>
+<div class="media-info-msg">화면 사이즈를 줄여주세요.</div>
+
+<style>
+</style>
